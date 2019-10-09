9d6560179987fd5995d691be64f0f43f054a9731
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-09 21:11:09 +0000

    build based on 56d8760

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Introduction-1","page":"Introduction","title":"Introduction","text":"","category":"section"},{"location":"#","page":"Introduction","title":"Introduction","text":"This is a place to collect random materials that explain mathematical and engineering ideas. ","category":"page"}]
+}
